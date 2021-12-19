2ee94e0778ddc6ef51c2acf8a3aa227c3aeb77b4
notprQn <90474257+notprQn@users.noreply.github.com>
2021-12-19 11:59:30 -0300

    Delete CalculadoraBonificacao.kt

--- CalculadoraBonificacao.kt
+++
@@ -1,9 +0,0 @@
-class CalculadoraBonificacao {
-
-    var total: Double = 0.0
-        private set
-
-    fun regsitrar(funcionario: Funcionario){
-        this.total += funcionario.bonificacao
-    }
-}
